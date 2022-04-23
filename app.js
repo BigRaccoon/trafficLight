daa6b341a2a2c515767532eb8dbee8c9802c26bb
BigRaccoon <55001607+BigRaccoon@users.noreply.github.com>
2022-04-23 11:49:54 +0300

    version 0.2

--- app.js
+++ app.js
@@ -1,68 +1,133 @@
-let aTop = document.querySelector('.top')
-let bMiddle = document.querySelector('.middle')
-let cBottom = document.querySelector('.bottom')
+const a = document.querySelector('.top') // upper signal
+const b = document.querySelector('.middle') // middle signal
+const c = document.querySelector('.bottom') // lower signal 
 
-let colors = ['red','yellow','green']
+const blockTF = document.querySelector('.trafficLight') // body of traffic Light
 
-// возвращает случайный элемент из массива
-let randomColor = (arr) => {
-  let rand = Math.floor(Math.random() * arr.length);
-  return arr[rand];
+const Colortmp = {
+   RED: 0,
+   YELLOW: 1,
+   GREEN: 2
 }
+
+const Color = {
+   RED: 'red',
+   YELLOW: 'yellow',
+   GREEN: 'green'
+}
+
+const colors = Object.values(Color);
+
 console.log(randomColor(colors))
 
-// задаем начальные значения
-while ( aTop.style.background===  bMiddle.style.background||  aTop.style.background===  cBottom.style.background||  cBottom.style.background===  bMiddle.style.background|| ( aTop.style.background=== 'red' &&  bMiddle.style.background=== 'yellow' &&  cBottom.style.background=='green')) {
-   aTop.style.background= randomColor(colors)
-   bMiddle.style.background= randomColor(colors)
-   cBottom.style.background= randomColor(colors)
-  console.log(aTop.style.background)
+document.querySelector('.btn').onclick = () => {
+   blockTF.style.pointerEvents = 'auto'
+
+   document.querySelector('.btn').style.display = 'none'
+   setTimeout(() => {
+      initializeColors()
+   }, 100)
+   
 }
 
-// выдаст индекс цвета 0 - красный 1 - желтый 2 - зеленый
-console.log(colors.indexOf(aTop.style.background))
+a.onclick = () => {
+   if (a.style.background === Color.GREEN) {
+      a.style.background = Color.RED
+   } else {
+      const colorIndex = colors.indexOf(a.style.background)
+      a.style.background = colors[colorIndex + 1]
+   }
+   if (b.style.background === Color.GREEN) {
+      b.style.background = Color.RED
+   } else {
+      const colorIndex = colors.indexOf(b.style.background)
+      b.style.background = colors[colorIndex + 1]
+   }
+   console.log(a.style.background, b.style.background, c.style.background)
+   if (checkValidColorSequence([a.style.background, b.style.background, c.style.background])) {
+      win()
+      reload()
+   }
+}
 
-// кликаем, чтобы менялись цвета
-aTop.onclick = () => {
-  if (colors.indexOf(aTop.style.background) === 2) {
-    aTop.style.background= colors[colors.indexOf(aTop.style.background)-2]
- } else {
-    aTop.style.background= colors[colors.indexOf(aTop.style.background)+1]
- }
- if (colors.indexOf(bMiddle.style.background) === 2){
-  bMiddle.style.background= colors[colors.indexOf(bMiddle.style.background)-2]
- } else {
-    bMiddle.style.background= colors[colors.indexOf(bMiddle.style.background)+2]
- }
- console.log(aTop.style.background,bMiddle.style.background,cBottom.style.background)
+b.onclick = () => {
+   if (b.style.background === Color.GREEN) {
+      b.style.background = Color.RED
+   } else {
+      const colorIndex = colors.indexOf(b.style.background)
+      b.style.background = colors[colorIndex + 1]
+   }
+   if (c.style.background === Color.RED) {
+      c.style.background = Color.GREEN
+   } else {
+      const colorIndex = colors.indexOf(c.style.background)
+      c.style.background = colors[colorIndex - 1]
+   }
+   console.log(a.style.background, b.style.background, c.style.background)
+   if (checkValidColorSequence([a.style.background, b.style.background, c.style.background])) {
+      win()
+      reload() 
+   }
+}
+
+c.onclick = () => {
+   if (c.style.background === Color.GREEN) {
+      c.style.background = Color.RED
+   } else {
+      const colorIndex = colors.indexOf(c.style.background)
+      c.style.background = colors[colorIndex + 1]
+   }
+   if (a.style.background === Color.RED) {
+      a.style.background = Color.GREEN
+   } else {
+      const colorIndex = colors.indexOf(a.style.background)
+      a.style.background = colors[colorIndex - 1]
+   }
+   console.log(a.style.background, b.style.background, c.style.background)
+   if (checkValidColorSequence([a.style.background, b.style.background, c.style.background])) {
+      win()
+      reload()
+   }
 
 }
 
-bMiddle.onclick = () => {
-  if (colors.indexOf(bMiddle.style.background) === 2) {
-    bMiddle.style.background= colors[colors.indexOf(bMiddle.style.background)-2]
- } else {
-    bMiddle.style.background= colors[colors.indexOf(bMiddle.style.background)+1]
- }
- if (colors.indexOf(cBottom.style.background) === 0){
-    cBottom.style.background= colors[colors.indexOf(cBottom.style.background)+2]
- } else {
-    cBottom.style.background= colors[colors.indexOf(cBottom.style.background)-1]
- }
- console.log(aTop.style.background,bMiddle.style.background,cBottom.style.background)
+console.log(a.style.background, b.style.background, c.style.background)
 
+function checkValidColorSequence(lightCircles) {
+   if (lightCircles.length !== 3) {
+      return false
+   }
+   console.log(lightCircles[0] == 'red' && lightCircles[1] == 'yellow' && lightCircles[2] == 'green');
+   console.log(lightCircles);
+   return lightCircles[0] == 'red' && lightCircles[1] == 'yellow' && lightCircles[2] == 'green';
 }
 
-cBottom.onclick = () => {
-  if (colors.indexOf(cBottom.style.background) === 2) {
-    cBottom.style.background= colors[colors.indexOf(cBottom.style.background)-2]
- } else {
-    cBottom.style.background= colors[colors.indexOf(cBottom.style.background)+1]
- }
- console.log(aTop.style.background,bMiddle.style.background,cBottom.style.background)
-
+function randomColor(arr) {
+   let rand = Math.floor(Math.random() * arr.length);
+   return arr[rand];
 }
 
-console.log(aTop.style.background,bMiddle.style.background,cBottom.style.background)
+function initializeColors() {
+   while (a.style.background === b.style.background || 
+      a.style.background === c.style.background || 
+      c.style.background === b.style.background || 
+      (a.style.background === 'red' && b.style.background === 'yellow' && c.style.background == 'green')) {
+      a.style.background = randomColor(colors)
+      b.style.background = randomColor(colors)
+      c.style.background = randomColor(colors)
+      console.log(a.style.background)
+   }
+}
 
 
+function reload() {
+   setTimeout(() => {
+      window.location.reload()
+   }, 1000)
+}
+
+function win() {
+   setTimeout(() => {
+      alert("YOU WIN!!!")
+   }, 100)
+}
